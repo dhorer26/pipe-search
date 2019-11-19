578dfceb45fd3a42688172032309f5576ee99dd1
Ravindra Dhore <ravindrad@ansh.com>
2019-11-19 14:22:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'firebase-auth-angular-app';
+  name = 'Angular-search';
+  users = [
+    {
+      full_name: 'Ravi Kumar',
+      address: 'South Delhi, New Delhi',
+      interests: 'playing guitar'
+    },
+    {
+      full_name: 'Rohit Kumar',
+      address: 'East Delhi, New Delhi',
+      interests: 'playing cricket'
+    },
+    {
+      full_name: 'Rani Kumari',
+      address: 'West Delhi, New Delhi',
+      interests: 'singing'
+    },
+    {
+      full_name: 'Vinita Kumari',
+      address: 'Central Delhi, New Delhi',
+      interests: 'dancing'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+import { FilterPipe } from './filter.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
